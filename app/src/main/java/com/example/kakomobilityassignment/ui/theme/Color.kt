68a3e4fac44b24aee5d18560157a469ac7853671
leeugun123 <leeugun123@naver.com>
2024-10-11 21:48:01 +0900

    feat : 도로 및 시간 거리 Box 색상 추가

--- app/src/main/java/com/example/kakomobilityassignment/ui/theme/Color.kt
+++ app/src/main/java/com/example/kakomobilityassignment/ui/theme/Color.kt
@@ -10,5 +10,15 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val departPlaceColor = Color(0xFF2196F3)
-val arrivePlaceColor = Color(0xFF4CAF50)
+val DepartPlaceColor = Color(0xFF2196F3)
+val ArrivePlaceColor = Color(0xFF4CAF50)
+
+val RouteUnknown = Color(0xE67F7E79)
+val RouteBlock = Color(0xFFFD382A) // Alpha 값을 포함하여 8자리로 표현하는 게 일반적입니다.
+val RouteJam = Color(0xFFFD382A)
+val RouteDelay = Color(0xFFF7AB17)
+val RouteSlow = Color(0xFFF0D300)
+val RouteNormal = Color(0xFF3BD72C)
+val StrokeColor = Color(0xFFFFFFFF)
+
+val TimeDistanceBoxColor = Color(0xFF625b71)
